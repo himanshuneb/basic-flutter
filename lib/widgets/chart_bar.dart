9ec73f709f9a3630d42566bd118513b7d04ecc70
Himanshu <himanshuneb@gmail.com>
2022-01-11 02:42:06 +0530

    Finished chart

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -2,11 +2,52 @@ import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
   //const ChartBar({ Key? key }) : super(key: key);
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      children: <Widget>[
+        FittedBox(child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          //We need to give this container a background that is partially filled
+          //Stack widget allows us to place elements on top of each other
+          child: Stack(
+            children: <Widget>[
+              //first widget here will be the bottom most widget
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPctOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
     );
   }
 }
